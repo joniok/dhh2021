0968628a005e46d2e7ef9bf97df001f62507fa08
Joni Oksanen <joni.oksanen@helsinki.fi>
2021-06-29 16:47:37 +0300

    First version of shiny app

--- electoral_districts/app.R
+++ electoral_districts/app.R
@@ -14,15 +14,20 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            
             selectInput(inputId = "time_period", label = h3("Select time period"), 
                         choices = list("Depression in Finland (1986-1995)" = "1986-1995", 
                                        "Global Financial Crisis (2004-2013)" = "2004-2013"), 
                         selected = 1),
             
+            sliderInput(inputId = "year", label = "Select year",
+                        min = 1986, max = 1995, value = 1990, sep = ""),
+            
+            
             selectInput(inputId = "groups", label = "Select parlamentary group", choices = NULL)
         ),
         
-        # Show a plot of the generated distribution
+        # Show a plot
         mainPanel(
             plotOutput("map_plot")
         )
@@ -32,27 +37,46 @@ ui <- fluidPage(
 # Define server logic 
 server <- function(input, output, session){
     
-    
-    selected_data <-  reactive({app_plot_data %>%
-            filter(period == input$time_period)
-    })
-    
     observeEvent(input$time_period, {
-        group_options <- selected_data() %>%
+        
+        group_options <- app_plot_data %>%
+            filter(period == input$time_period) %>%
+            filter(year == input$year) %>%
             mutate(party = droplevels(party)) %>%
             pull(party) %>%
             levels()
         
         updateSelectInput(session = session, inputId = "groups", choices = group_options)
         
+        if(input$time_period == "2004-2013"){
+            updateSliderInput(inputId = "year", min = 2004, max = 2013, value = 2008)
+        } else {
+            updateSliderInput(inputId = "year", min = 1986, max = 1995, value = 1990)
+        }
+        
     })
     
     
+    plot_data <- reactive({
+        app_plot_data %>%
+            filter(period == input$time_period) %>%
+            filter(year == input$year) %>%
+            filter(party == input$groups)
+    })
+    
+    global_limits <- reactive({
+        c(0, max(as.data.frame(app_plot_data$speaker_district_count)))
+        
+    })
     
     
     output$map_plot <- renderPlot({
-        ggplot(selected_data(), aes(fill = mention_count.x)) + 
-            geom_sf(colour = alpha("white", 1/3), fill = )
+        ggplot() + 
+            geom_sf(data = plot_data(), 
+                    aes_string(fill = "speaker_district_count",
+                               geometry = "geom",), 
+                    colour = alpha("white", 1/3)) +
+            viridis::scale_fill_viridis(limits = global_limits())
     })
 }
 
