6d26c1c74b323b438083120753a4fe0410f21866
Joni Oksanen <joni.oksanen@helsinki.fi>
2021-07-06 11:54:49 +0300

    Add parlamentary groups

--- data_preprocess.R
+++ data_preprocess.R
@@ -23,7 +23,11 @@ names(df_list) <- stringr::str_replace_all(data_files, "data//|.csv", "")
 # merge groups and district data
 historical_names <- read.csv("historical_electoral_districts.csv", stringsAsFactors = FALSE)
 
+
+#########################
+
 # Merge data sets
+
 ## electoral districts 
 
 ### Main data
@@ -69,26 +73,45 @@ districts <- districts_raw %>%
 
 saveRDS(districts, file = paste0(out_path, "districts.rds"))
 
-
+#########################
 
 ## parliamentary groups 
 parl_groups_raw <- list(`2004-2013` = df_list[["city_counts_statistics_by_parties_04_13"]],
                         `1986-1995` = df_list[["city_counts_statistics_by_parties_86_95"]]) %>%
   purrr::map_df(I, .id = "period") %>%
   filter(!grepl("Ahvenanmaa", party)) %>%# Exclude Åland representatives
+  filter(city != "Maarianhamina") %>%
   rename(speaker_party_count = mention_count) %>%
   left_join(d1, by = "city") %>%
   st_as_sf() # convert to shape file
 
 
-### aggregate to 
-parl_groups <- parl_groups_raw %>%
-  districts <- districts_raw %>%
-  group_by(period, year, party) %>%
-  summarise_at(.vars = "speaker_party_count", .funs = sum ) %>%
-  select(-vaalipiiri_vuosi, -vaalipiiri_code, -kunta)
-
 ### excluded cities 
+excluded_cities_parl_groups <- list(period = unique(parl_groups_raw$period),
+                                  year = unique(parl_groups_raw$year),
+                                  party = unique(parl_groups_raw$party),
+                                  city = d1$city
+) %>% 
+  purrr::cross() %>%
+  plyr::ldply(data.frame) %>%
+  mutate(speaker_party_count = 0) %>%
+  anti_join(parl_groups_raw, by = c("period", "year", "city", "party")) %>%
+  left_join(d1, by = "city") %>%
+  st_as_sf()
 
+### Add annual counts
+parl_group_counts <- parl_groups_raw %>%
+  tibble() %>%
+  group_by(period, year, city) %>%
+  summarise(speaker_party_total = sum(speaker_party_count, na.rm=TRUE), .groups = "keep")
+
+
+### aggregate to electoral level
+parl_groups <- parl_groups_raw %>%
+  bind_rows(excluded_cities_parl_groups) %>%
+  left_join(parl_group_counts, by = c("period", "year", "city")) %>%
+  st_as_sf() # convert to shape file
+
+saveRDS(parl_groups, file = paste0(out_path, "parl_groups.rds"))
 
 
